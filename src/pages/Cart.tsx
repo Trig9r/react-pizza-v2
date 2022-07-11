5060b6257cbc243fd22ee8e7c1b48a2b2ed73edb
Kirill <example@gmail.com>
2022-07-11 21:35:08 +0300

    qwe

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -4,6 +4,12 @@ import { useSelector, useDispatch } from 'react-redux';
 import { CartItem, CartEmpty } from '../components';
 import { clearItems } from '../redux/cart/slice';
 import { selectCart } from '../redux/cart/selectors';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
 const Cart: React.FC = () => {
   const dispatch = useDispatch();
@@ -11,10 +17,19 @@ const Cart: React.FC = () => {
   const { items, totalPrice } = useSelector(selectCart);
   const totalCount = items.reduce((count: number, currentObj: any) => count + currentObj.count, 0);
 
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   const onClickClear = () => {
-    if (window.confirm('Вы действительно хотите очистить корзину?')) {
-      dispatch(clearItems());
-    }
+    dispatch(clearItems());
+    setOpen(false);
   };
 
   if (!totalCount) {
@@ -56,45 +71,28 @@ const Cart: React.FC = () => {
             </svg>
             Корзина
           </h2>
-          <button onClick={onClickClear} className="cart__clear">
-            <svg
-              width="20"
-              height="20"
-              viewBox="0 0 20 20"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg">
-              <path
-                d="M2.5 5H4.16667H17.5"
-                stroke="#B6B6B6"
-                strokeWidth="1.2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-              <path
-                d="M6.66663 5.00001V3.33334C6.66663 2.89131 6.84222 2.46739 7.15478 2.15483C7.46734 1.84227 7.89127 1.66667 8.33329 1.66667H11.6666C12.1087 1.66667 12.5326 1.84227 12.8451 2.15483C13.1577 2.46739 13.3333 2.89131 13.3333 3.33334V5.00001M15.8333 5.00001V16.6667C15.8333 17.1087 15.6577 17.5326 15.3451 17.8452C15.0326 18.1577 14.6087 18.3333 14.1666 18.3333H5.83329C5.39127 18.3333 4.96734 18.1577 4.65478 17.8452C4.34222 17.5326 4.16663 17.1087 4.16663 16.6667V5.00001H15.8333Z"
-                stroke="#B6B6B6"
-                strokeWidth="1.2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-              <path
-                d="M8.33337 9.16667V14.1667"
-                stroke="#B6B6B6"
-                strokeWidth="1.2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-              <path
-                d="M11.6666 9.16667V14.1667"
-                stroke="#B6B6B6"
-                strokeWidth="1.2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-
-            <span>Очистить корзину</span>
-          </button>
+          <Button variant="outlined" onClick={handleClickOpen}>
+            Очистить корзину
+          </Button>
+          <Dialog
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description">
+            <DialogTitle id="alert-dialog-title">{"Use Google's location service?"}</DialogTitle>
+            <DialogContent>
+              <DialogContentText id="alert-dialog-description">
+                Let Google help apps determine location. This means sending anonymous location data
+                to Google, even when no apps are running.
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleClose}>Disagree</Button>
+              <Button onClick={onClickClear} autoFocus>
+                Agree
+              </Button>
+            </DialogActions>
+          </Dialog>
         </div>
         <div className="content__items">
           {items.map((pizza: any, id: number) => (
